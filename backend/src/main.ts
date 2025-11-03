b698739277119908a82df4bf0196fbef630c9d4b
SamuelVSantos <samuel@datarudder.com>
2025-11-03 20:51:19 -0300

    integração back front

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,18 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Habilitar CORS
+  app.enableCors({
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:3000',
+      'http://localhost:4173',
+    ],
+    methods: ['POST'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -15,4 +27,4 @@ async function bootstrap() {
 
   await app.listen(3000);
 }
-bootstrap();
+bootstrap().catch(console.error);
